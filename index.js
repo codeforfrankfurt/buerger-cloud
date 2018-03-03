fba57cabca99a554311c83c095981ab95287e653
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-03-03 15:04:13 +0100

    Use environment for port number. Added some example routes

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/health', (req, res) => res.json('ok'))
-app.listen(8080)
+
+app.post('/meter/:meter/loc/:lat/:lon', (req, res) => {
+  res.json('not yet implemented')
+})
+
+app.get('/data/:lat1/:lon1/:lat2/:lon2/:from/:to', (req, res) => {
+  res.json('not yet implemented')
+})
+
+app.listen(process.env.PORT || 8080)
