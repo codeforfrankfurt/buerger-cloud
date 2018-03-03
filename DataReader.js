9932b63ea3f7d399eac5871ec673337212056a60
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-03-03 18:31:11 +0100

    Fixed return data structure like specified in https://hackmd.io/BwVgDA7AbBCmBGBaCBGFBORAWAxigJovFOmIurvGLDgGa10pA===?view#Data-structure

--- DataReader.js
+++ DataReader.js
@@ -1,9 +1,27 @@
 const fs = require('fs.promised')
+const path = require('path')
+
+const dataPath = __dirname + '/data'
+
+function inBox(file, box) {
+  return true
+}
+
+function inDateTimeRange(file, dateTimeRange) {
+  return true
+}
+
+function filterRelevant(file, box, dateTimeRange) {
+  return file.indexOf('.') !== 0 && inBox(file, box) && inDateTimeRange(file, dateTimeRange)
+}
 
 module.exports = {
   find: params => {
-    return fs.readdir(__dirname + '/data')
-      .then(files => files.filter(file => file.indexOf('.') !== 0))
-      .then(files => files.map(file => JSON.parse(fs.readFileSync(__dirname + '/data/' + file).toString())))
+    const box = [params.lat1, params.lon1, params.lat2, params.lon2]
+    const dateTimeRange = {from: params.from, to: params.to}
+    return fs.readdir(dataPath)
+      .then(files => files.filter(file => filterRelevant(file, box, dateTimeRange)))
+      .then(files => files.map(file => JSON.parse(fs.readFileSync(path.join(dataPath, file)).toString())))
+      .then(points => ({box, dateTimeRange, points}))
   }
 }
