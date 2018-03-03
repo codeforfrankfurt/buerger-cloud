97fc6460136db492256715b2c4ed893964fd4193
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-03-03 17:52:38 +0100

    Use another lib for CORS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('express-cors')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const MeasurementHandler = require('./MeasurementHandler')
 const DataReader = require('./DataReader')
@@ -7,7 +7,7 @@ const DataReader = require('./DataReader')
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use(cors({allowedOrigins: ['*']}))
+app.use(cors())
 
 app.get('/health', (req, res) => res.json('ok'))
 
